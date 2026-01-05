882c5dbf6ffa7e3fbe46bd90ca14dfd943b277ba
Kirill Magurin <55835923+Pickle-Pee@users.noreply.github.com>
2026-01-05 14:14:33 +0300

    Rebrand app to "Цифровой класс"

--- lib/app.dart
+++ lib/app.dart
@@ -10,7 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Школьник · Информатика',
+      title: 'Цифровой класс',
       theme: AppTheme.lightTheme,
       initialRoute: initialRoute,
       routes: appRoutes,
